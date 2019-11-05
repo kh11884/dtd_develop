594a5cf9b7378178df7745a2fe436b6486d78be3
kh11884 <kh11884@ngs.ru>
2019-11-05 12:59:45 +0700

    Задача JCObject добавил массив объектов-стран

--- JSObjects/src/objects.js
+++ JSObjects/src/objects.js
@@ -1,121 +1,41 @@
-var countries = [];
-var country = {
-name: "Франция",
-cities:
-}
-countries.push(country);
-
-country.name;
-country.cities = [{name:"Париж"}];
-
-var paris = {
-name: "Париж",
-population: 2206488
-}
-var marcel = {
-name: "Марсель",
-population: 870018
-}
-var toulouse = {
-name: "Тулуза",
-population: 482738
-}
-var nice = {
-name: "Ницца",
-population: 533554
-}
-var lion = {
-name: "Лион",
-population: 506615
-}
-//---------------------------
-
-var berlin = {
-name: "Берлин",
-population: 6010910
-}
-var hanover = {
-name: "Гановер",
-population: 535061
-}
-var frankfurt = {
-name: "Франкфурт",
-population: 746878
-}
-var munich = {
-name: "Мюнхен",
-population: 1456039
-}
-var ingolstadt = {
-name: "Ингольштадт",
-population: 137072
-}
-
-//---------------------------
-
-var rome = {
-name: "Рим",
-population: 2872800
-}
-var milan = {
-name: "Милан",
-population: 1366180
-}
-var venice = {
-name: "Венеция",
-population: 260203
-}
-var florence = {
-name: "Флоренция",
-population: 380948
-}
-var pisa = {
-name: "Пиза",
-population: 90118
-}
-
-//---------------------------
-
-var madrid = {
-name: "Мадрид",
-population: 3165235
-}
-var barcelona = {
-name: "Барселона",
-population: 1602386
-}
-var valencia = {
-name: "Валенсия",
-population: 786 424
-}
-var palma = {
-name: "Пальма-де-Майорка",
-population: 399093
-}
-var sevilla = {
-name: "Севилья",
-population: 696676
-}
-
-//---------------------------
-
-var london = {
-name: "Лондон",
-population: 8825000
-}
-var liverpool = {
-name: "Ливерпуль",
-population: 491500
-}
-var manchester = {
-name: "Манчестер",
-population: 545500
-}
-var bath = {
-name: "Бат",
-population: 94782
-}
-var windsor = {
-name: "Виндзор",
-population: 28324
-}
+var countries = [{
+    name: "Франция", cities: [
+        {name: "Париж", population: 2206488},
+        {name: "Марсель", population: 870018},
+        {name: "Тулуза", population: 482738},
+        {name: "Ницца", population: 533554},
+        {name: "Лион", population: 506615}]
+},
+    {
+        name: "Германия", cities: [
+            {name: "Берлин", population: 6010910},
+            {name: "Гановер", population: 535061},
+            {name: "Франкфурт", population: 746878},
+            {name: "Мюнхен", population: 1456039},
+            {name: "Ингольштадт", population: 137072}]
+    },
+    {
+        name: "Италия", cities: [
+            {name: "Рим", population: 2872800},
+            {name: "Милан", population: 1366180},
+            {name: "Венеция", population: 260203},
+            {name: "Флоренция", population: 380948},
+            {name: "Пиза", population: 90118}]
+    },
+    {
+        name: "Испания", cities: [
+            {name: "Мадрид", population: 3165235},
+            {name: "Барселона", population: 1602386},
+            {name: "Валенсия", population: 786424},
+            {name: "Пальма-де-Майорка", population: 399093},
+            {name: "Севилья", population: 696676}]
+    },
+    {
+        name: "Великобритания", cities: [
+            {name: "Лондон", population: 8825000},
+            {name: "Ливерпуль", population: 491500},
+            {name: "Манчестер", population: 545500},
+            {name: "Бат", population: 94782},
+            {name: "Виндзор", population: 28324}]
+    }
+];
