ec8c2f17cc6fc59f6552e2ded4e3a3eb5e939858
kh11884 <kh11884@ngs.ru>
2020-01-06 19:31:58 +0700

    Добавлена модель калькулятора валют (расчет таможенной стоимости)

--- 1_forTest/src/js/test_1.js
+++ 1_forTest/src/js/test_1.js
@@ -5,8 +5,10 @@ function moneyFormat(n) {
 var CurrencyCalculator = new Vue({
   el: '#CurrencyCalculator',
   data: {
-    usd_rate: 0,
-    eur_rate: 0,
+    hand_usd_rate: 0,
+    hand_eur_rate: 0,
+    cbr_usd_rate: 0,
+    cbr_eur_rate: 0,
     usd_charges: "",
     eur_charges: "",
     rub_charges: "",
@@ -23,14 +25,28 @@ var CurrencyCalculator = new Vue({
 
     return moneyFormat(result);
     },
+    usd_rate: function(){
+    return this.isHandMadeRates ? this.hand_usd_rate : this.cbr_usd_rate;
+    },
+    eur_rate: function(){
+    return this.isHandMadeRates ? this.hand_eur_rate : this.cbr_eur_rate;
+    },
+    isInvalid_hand_usd_rate: function(){
+          return this.hand_usd_rate === "";
+           },
+    isInvalid_hand_eur_rate:  function(){
+           return this.hand_eur_rate === "";
+           },
   }
 })
 
 var options = {year: 'numeric', month: 'short', day: 'numeric'};
 
 $.getJSON("https://www.cbr-xml-daily.ru/daily_json.js", function(data) {
-    CurrencyCalculator.usd_rate = data.Valute.USD.Value;
-    CurrencyCalculator.eur_rate = data.Valute.EUR.Value;
+    CurrencyCalculator.cbr_usd_rate = data.Valute.USD.Value;
+    CurrencyCalculator.cbr_eur_rate = data.Valute.EUR.Value;
     console.log(data.Date);
     CurrencyCalculator.date = data.Date.substring(0, 10);
+    CurrencyCalculator.hand_usd_rate = CurrencyCalculator.cbr_usd_rate;
+    CurrencyCalculator.hand_eur_rate = CurrencyCalculator.cbr_eur_rate;
 });
