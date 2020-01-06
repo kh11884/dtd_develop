6f2931fae0296a0b1e8b328c0b9071ffa74e3d4e
kh11884 <kh11884@gmail.com>
2020-01-07 01:19:02 +0700

    добработана страничка с калькулятором таможенной стоимости в custom_helper

--- customs_helper/vue.config.js
+++ customs_helper/vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  'publicPath': "/test_web/"  // эта настройка для простого продакшена, без сервера, чтобы запустилось просто в браузере
 }
