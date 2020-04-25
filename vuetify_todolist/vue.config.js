dbdb7b8060e396bc37f6b564acb0b39fa45e6552
kh11884 <kh11884@ngs.ru>
2020-04-25 09:57:53 +0700

    выделение проекта в отдельный репозиторий

--- vuetify_todolist/vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'transpileDependencies': [
-    'vuetify'
-  ],
-  'lintOnSave': false,
-  // 'publicPath': './'  // эта настройка для простого продакшена, без сервера, чтобы запустилось просто в браузере
-}
